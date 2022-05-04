d7806713ce0b2b40e7c501fae88d43b0903dfcaa
efgarro <efgarro@yahoo.com>
2022-05-03 23:32:11 -0600

    00-practice 4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
+import ControlledInput from "./ControlledInput";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h4>Hellow Bella</h4>
       <MyComponent />
       <Counter />
+      <ControlledInput />
     </main>
   );
 }
